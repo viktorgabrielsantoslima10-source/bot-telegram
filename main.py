45512df0671187fd33239569d55809e98611107b
viktorgabrielsantoslima10-source <viktorgabrielsantoslima10@gmail.com>
2026-08-26 13:58:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import discord
 import os
 from discord.ext import commands
+import datetime
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+intents.guilds = True
+
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
-    print(f"Bot {bot.user} online!")
+    await bot.tree.sync()
+    await bot.change_presence(
+        activity=discord.Game(name="🔥 ROBLOX | Creigh_Tps1 ONLINE 24HRS 🔥"),
+        status=discord.Status.online
+    )
+    print(f"🔥 {bot.user} ONLINE!")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong!")
-
-bot.run(os.getenv("DISCORD_TOKEN"))
+# ===== /AMISTOSO =====
+@bot.tree.command(name="amistoso", description="⚽💥 MARCAR AMISTOSO BRABO")
+async def amistoso(interaction: discord.Interaction, adversario: str, data: str, horario: str):
+    embed = discord.Embed(
+        title="━━━━━━━━━ ⚽🔥💥 AMISTOSO OFICIAL 💥🔥⚽ ━━━━━━━━━",
+        description=f"""
+```diff
++ 🏆🏆🏆 CONFRONTO CONFIRMADO 🏆🏆🏆
